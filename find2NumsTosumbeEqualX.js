e02b92b3d5b780688d509d8a0a87987adbc1057d
Maja Gach <majaanna94@gmail.com>
2020-06-01 12:39:27 +0200

    find2NumsToSumBeEqual to x faster solution one loop

--- find2NumsTosumbeEqualX.js
+++ find2NumsTosumbeEqualX.js
@@ -1,14 +1,31 @@
-let numbers = [2, 3, 4, 2];
-let desiredSum = 4;
+// let numbers = [2, 3, 4, 2];
+// let desiredSum = 4;
 
-function twoNumsSum(numbers, sum) {
-  for (let i = 0; i < numbers.length; i++) {
-    for (let j = 0; j < numbers.length; j++) {
-      if (numbers[i] + numbers[j] === sum && j !== i) {
-        return [numbers[i], numbers[j]];
-      }
+// function twoNumsSum(numbers, sum) {
+//   for (let i = 0; i < numbers.length; i++) {
+//     for (let j = 0; j < numbers.length; j++) {
+//       if (numbers[i] + numbers[j] === sum && j !== i) {
+//         return [numbers[i], numbers[j]];
+//       }
+//     }
+//   }
+// }
+
+// console.log(twoNumsSum(numbers, desiredSum));
+
+function twoSum(nums, target) {
+  const previousValues = {};
+  for (let i = 0; i < nums.length; i++) {
+    const currentNumber = nums[i];
+    const neededValue = target - currentNumber;
+
+    if (previousValues.toString().indexOf(neededValue) == -1) {
+      return [neededValue, nums[i]];
+    } else {
+      previousValues[currentNumber] = nums[i];
+      // hash[key] = value
     }
   }
 }
 
-console.log(twoNumsSum(numbers, desiredSum));
+console.log(twoSum([2, 2, 4, 5], 4));
