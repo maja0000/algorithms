d0cce856ca1b7bd0b6829e997ffc2549608bd6e2
Maja Gach <majaanna94@gmail.com>
2020-06-26 10:00:24 +0200

    symmetric difference

--- SymmetricDifference.js
+++ SymmetricDifference.js
@@ -11,3 +11,14 @@ const diff = (arr1, arr2) => [
 const sym = (...args) => [...new Set(args.reduce(diff))];
 
 sym([1, 2, 3], [5, 2, 1, 4]);
+
+//function diffArray(arr1, arr2) {
+//   let newArr =[]
+//   let arr1unique = arr1.filter((e) => !arr2.includes(e))
+//   let arr2unique = arr2.filter((e) => !arr1.includes(e))
+// newArr.push(arr1unique,arr2unique)
+//   let flat  = newArr.flat();
+
+//   return flat;
+// }
+// diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
